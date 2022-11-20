01a8717a6247ad8539df35f2e0ecb5c9c551f2d0
legendofnothing <huyrtb123@gmail.com>
2022-11-20 21:06:29 +0700

    Add Enemy Bullet and Bases

---
+++ Assets/_src/Scripts/Enemy/EnemyWeapon/EnemyBullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using _src.Scripts.Core.EventDispatcher;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _src.Scripts.Enemy.EnemyWeapon
+{
+    public class EnemyBullet : MonoBehaviour
+    {
+        [HideInInspector] public float damage;
+        
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            StartCoroutine(DestroyBullet());
+        }
+
+        private IEnumerator DestroyBullet()
+        {
+            yield return new WaitForSeconds(0.1f);
+            
+            this.SendMessage(EventType.EnemyDamagePlayer, damage);
+
+            transform.DOKill(this);
+            Destroy(gameObject);
+        }
+    }
+}
