7f20fc99c9e58eb0d0d5f06af9488d27f9421b18
legendofnothing <huyrtb123@gmail.com>
2023-05-15 17:11:06 +0700

    Basic ShopUI Implementation

--- Assets/_src/Scripts/UI/InGame/ShopUI.cs
+++ Assets/_src/Scripts/UI/InGame/ShopUI.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UIElements;
+using EventDispatcher = _src.Scripts.Core.EventDispatcher.EventDispatcher;
+using Image = UnityEngine.UI.Image;
 
-public class ShopUI : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace _src.Scripts.UI.InGame {
+    public class ShopUI : MonoBehaviour {
+        [Header("Refs")]
+        public GameObject container;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Misc")] 
+        public GameObject aura;
+
+        private Sequence _currAuraSequence;
+
+        private void Start() {
+            container.SetActive(false);
+            EventDispatcher.instance.SubscribeListener(EventType.OpenShop, _=>OpenShop());
+        }
+
+        private void OpenShop() {
+            container.SetActive(true);
+            _currAuraSequence = DOTween.Sequence();
+            _currAuraSequence
+                .Append(aura.transform.DOScale(new Vector3(1.1f, 1.2f), 1.4f))
+                .SetLoops(-1, LoopType.Yoyo);
+        }
+
+        public void OnProceed() {
+            _currAuraSequence.Kill();
+            container.SetActive(false);
+            EventDispatcher.instance.SendMessage(EventType.SwitchToPlayer);
+            StartCoroutine(DelayInput());
+        }
+
+        private IEnumerator DelayInput() {
+            Player.Player.instance.input.CanInput(false);
+            yield return new WaitForSeconds(0.4f);
+            Player.Player.instance.input.CanInput(true);
+        }
     }
 }
