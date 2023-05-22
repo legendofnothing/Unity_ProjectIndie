da4423a0aec85c6ffab81151c56d8ae3f1a6653a
legendofnothing <huyrtb123@gmail.com>
2023-05-22 17:02:27 +0700

    [UI]

--- Assets/Scripts/UI/InGame/GameUI.cs
+++ Assets/Scripts/UI/InGame/GameUI.cs
@@ -4,13 +4,39 @@ using UnityEngine.UI;
 
 namespace UI.InGame {
     public class GameUI : MonoBehaviour {
-        [Header("Canvas Related")] 
-        public CanvasScaler scaler;
+        [Header("UI")] 
+        public GameObject footerUI;
+
+        public RectTransform canvasRect;
+        private CanvasScaler _scaler;
+        private Player.Player _player;
 
         private void Awake() {
+            canvasRect = GetComponent<RectTransform>();
+            _scaler = GetComponent<CanvasScaler>();
+            
             var ratio = Screen.width / (float)Screen.height;
             if (Mathf.Approximately(ratio, 1080f / 1920f)) return;
-            scaler.matchWidthOrHeight = Screen.width / (float)Screen.height;
+            _scaler.matchWidthOrHeight = Screen.width / (float)Screen.height;
+        }
+
+        private void Start() {
+            _player = Player.Player.instance;
+            
+            //Set footer to desired position
+            var pos 
+                = _player.playerCamera
+                    .WorldToScreenPoint(_player.transform.position);
+            pos.z = 0;
+            RectTransformUtility
+                .ScreenPointToLocalPointInRectangle(
+                    canvasRect,
+                    pos,
+                    _player.playerCamera,
+                    out var desired
+                );
+
+            footerUI.transform.localPosition = desired;
         }
     }
 }
